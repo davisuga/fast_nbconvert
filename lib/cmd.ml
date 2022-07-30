95c3c75be63fdc2fc9833bf1bebb1307fad73016
Davi Suga <daviciencia1@gmail.com>
2022-07-30 14:04:32 -0300

    added cmd parsing

--- lib/cmd.ml
+++ lib/cmd.ml
@@ -1,1 +1,34 @@
-let run () = Parse.make_py_file Sys.argv.(1) |> ignore
+open Cmdliner
+
+let files_arg =
+  Arg.(
+    non_empty
+    & pos_all file []
+    & info [] ~docv:"FILES" ~doc:"The paths to the source files")
+
+let make_file_flag =
+  Arg.(
+    last
+    & vflag_all [ false ]
+        [
+          ( true,
+            info ~doc:"Creates a file instead of printing to stdout"
+              [ "make-file"; "mk" ] );
+        ])
+
+let command_term =
+  Term.(
+    const (fun paths create_file ->
+        List.map (Parse.make_py_file ~create_file) paths |> ignore)
+    $ files_arg
+    $ make_file_flag)
+
+let main_command =
+  let command_documentation =
+    Cmd.info "Fast NB Converter"
+      ~doc:"Parses Jupyter Notebooks into code and vice versa!"
+  in
+
+  Cmd.v command_documentation command_term
+
+let run () = exit (Cmd.eval main_command)

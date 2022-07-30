abf87fbd4645c88e9b5f9174094ed5bff2de7ac9
Davi Suga <daviciencia1@gmail.com>
2022-07-30 14:05:47 -0300

    removed unused code

--- lib/parse.ml
+++ lib/parse.ml
@@ -1,12 +1,12 @@
-let open_file path env = Eio.Dir.load (Eio.Stdenv.cwd env) path
+(* let open_file path env = Eio.Dir.load (Eio.Stdenv.cwd env) path *)
 
 let get_sources_paths ext path =
   Utils.dir_contents path |> List.filter (String.ends_with ~suffix:ext)
 
 let with_cell_counter num cell = Printf.sprintf "# In[%i]\n%s" num cell
-let create_file = try Sys.argv.(2) = "--make-file" with _ -> false
+(* let create_file = try Sys.argv.(2) = "--make-file" with _ -> false *)
 
-let make_py_file path =
+let make_py_file ~create_file path =
   let open Ipynb_types in
   let open Ppx_deriving_yojson_runtime in
   ( Yojson.Safe.from_file path

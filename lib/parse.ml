1f336dd6be61bee281afe6df86f17cd17d4eecec
Davi Suga <daviciencia1@gmail.com>
2022-11-04 12:16:51 -0300

    feat: clean parser

--- lib/parse.ml
+++ lib/parse.ml
@@ -6,39 +6,51 @@ let get_sources_paths ext path =
 let with_cell_counter num cell = Printf.sprintf "# In[%i]\n%s" num cell
 (* let create_file = try Sys.argv.(2) = "--make-file" with _ -> false *)
 
+let print_py_code_cell source index =
+  with_cell_counter index
+    (String.concat ""
+       (source
+       |> List.map (fun src ->
+              if Utils.contains src "!pip" then "# " ^ src else src)))
+
+let print_py_md_cell source index =
+  with_cell_counter index
+    (String.concat "" (source |> List.map (fun s -> "# " ^ s)))
+
+let (print_py_cell : int -> Ipynb_types.cell -> string) =
+ fun index { cell_type; source } ->
+  match cell_type with
+  | "code" -> print_py_code_cell source index
+  | "markdown" -> print_py_md_cell source index
+  | _ -> ""
+
+let get_py_file_path path =
+  (path
+  |> String.split_on_char '.'
+  |> Base.List.drop_last
+  |> Option.value ~default:[]
+  |> String.concat ".")
+  ^ ".py"
+
+let write_py_files path =
+  open_out
+    ((path
+     |> String.split_on_char '.'
+     |> Base.List.drop_last
+     |> Option.value ~default:[]
+     |> String.concat ".")
+    ^ ".py")
+
 let make_py_file ~create_file path =
   let open Ipynb_types in
   let open Ppx_deriving_yojson_runtime in
   ( Yojson.Safe.from_file path
   |> python_notebook_of_yojson
-  >|= (fun notebook ->
-        notebook.cells
-        |> List.mapi (fun index { cell_type; source } ->
-               match cell_type with
-               | "code" ->
-                   with_cell_counter index
-                     (String.concat ""
-                        (source
-                        |> List.map (fun src ->
-                               if Utils.contains src "!pip" then "# " ^ src
-                               else src)))
-               | "markdown" ->
-                   with_cell_counter index
-                     (String.concat "" (source |> List.map (fun s -> "# " ^ s)))
-               | _ -> ""))
+  >|= (fun notebook -> notebook.cells |> List.mapi print_py_cell)
   >|= String.concat "\n"
   >|= fun result ->
-    if create_file then
-      output_string
-        (open_out
-           ((path
-            |> String.split_on_char '.'
-            |> Base.List.drop_last
-            |> Option.value ~default:[]
-            |> String.concat ".")
-           ^ ".py"))
-        result
+    if create_file then output_string (open_out (get_py_file_path path)) result
     else print_string result )
   |> function
   | Ok _ -> "nicee"
-  | Error e -> failwith ("Fuck " ^ e)
+  | Error e -> failwith ("Oops " ^ e)
